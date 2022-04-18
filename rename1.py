0791cf86da2451c4c2a43849358246825cf7ce07
yangshuang <yangshuang19970521@163.com>
2022-04-18 22:06:33 +0800

    4.18

--- rename1.py
+++ rename1.py
@@ -1,21 +1,27 @@
 import os,sys
-path='/Users/yangshuang/Desktop/3000_dataset1/VOCdevkit/VOC2007/JPEGImages'
+path='/Users/yangshuang/Desktop/3000_dataset1/3000_test/3000_test_img/'
 filelist=os.listdir(path)
 count=len(filelist)
 # train_list=filelist[:int(count*0.9)]
 # val_list=filelist[int(count*0.9):]
 
+# with open("train.txt","w") as f:
+#     with open('val.txt','w') as f1:
+#         for i in range(len(filelist)):
+#             filename=filelist[i].split('.')[0]#0代表取.前面的部分
+#             x=filelist[i].split('.')[1]
+#             if x=='jpg':
+#                 if filename[0]!='V':
+#                     f.write(filename+'\n')
+#                 else:
+#                     f1.write(filename+'\n')
+#             else:
+#                 print(filelist[i])
 
-with open("train.txt","w") as f:
-    with open('val.txt','w') as f1:
-        for i in range(len(filelist)):
-            filename=filelist[i].split('.')[0]#0代表取.前面的部分
-            x=filelist[i].split('.')[1]
-            if x=='jpg':
-                if filename[0]!='V':
-                    f.write(filename+'\n')
-                else:
-                    f1.write(filename+'\n')
-            else:
-                print(filelist[i])
+with open('test.txt','w') as f:
+    for i in range(len(filelist)):
+        x =filelist[i].split('.')[1]
+        filename=filelist[i].split('.')[0]
 
+        if x=='jpg':
+            f.write(filename+'\n')
